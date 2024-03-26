9f297e49983ecaa8c3dbaab2fbfe268f4af1f606
Joshua Ow <joshuaow@gmail.com>
2024-03-26 08:06:31 -0500

    chore: intitialized spring boot app for admin server, generated root level git ignore file with gitignore.io (SCRUM-13)

---
+++ admin_server/src/test/java/com/backend/admin_server/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.admin_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
