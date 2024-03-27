fe85165e5dc5bd31aaa65095aa0fefd3de065d21
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-03-26 20:35:07 -0400

    add the heading and nav to dashborad

--- wombat-admin/src/components/dashboard.tsx
+++ wombat-admin/src/components/dashboard.tsx
@@ -1,5 +1,6 @@
 import DailyVisits from "./daily_visits";
 import IncomingRequests from "./incoming_requests";
+import Statistics from "./statistics";
 
 
 
@@ -25,7 +26,32 @@ export default function Dashboard() {
 
             <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
                 <IncomingRequests />
-                <DailyVisits />
+
+                <h1 className="text-lg font-semibold leading-6 text-zinc-800 mt-10 mb-4">Reports</h1>
+
+                <div className="sm:hidden">
+                    <label htmlFor="tabs" className="sr-only">Select a tab</label>
+                    {/* <!-- Use an "onChange" listener to redirect the user to the selected tab URL. --> */}
+                    <select id="tabs" name="tabs" className="block w-full rounded-md border-zinc-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+                        <option>Daily Visits</option>
+                        <option>User Audit Trail</option>
+                        <option selected>Statistics</option>
+
+                    </select>
+                </div>
+                <div className="hidden sm:block">
+                    <div className="">
+                        <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+                            {/* <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-zinc-500 hover:border-zinc-300 hover:text-zinc-700" --> */}
+                            <a href="#" className=" border-teal-500 text-teal-600  hover:border-zinc-300 hover:text-zinc-700 whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium">Daily Visits</a>
+                            <a href="#" className="border-transparent text-zinc-500 hover:border-zinc-300 hover:text-zinc-700 whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium">User Audit Trail</a>
+                            <a href="#" className="border-transparent text-zinc-500 whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium" aria-current="page">Statistics</a>
+
+                        </nav>
+                    </div>
+                </div>
+                {/* <DailyVisits /> */}
+                <Statistics />
             </div>
 
         </div>
