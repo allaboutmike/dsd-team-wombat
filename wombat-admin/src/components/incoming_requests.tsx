a544132d3c8fbad63d4abfdfb2a05733f05bc40d
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-03-28 16:50:43 -0400

    WIP: update styling

--- wombat-admin/src/components/incoming_requests.tsx
+++ wombat-admin/src/components/incoming_requests.tsx
@@ -1,21 +1,12 @@
+import Statistics from "./statistics";
 
 
 export default function IncomingRequests() {
     return (
         <div>
-            <div className="sm:flex sm:items-center my-4">
-                <div className="sm:flex-auto">
-                    <h1 className="text-lg font-semibold leading-6 text-zinc-800">Requests</h1>
-
-                </div>
-                <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                    <button type="button" className="block rounded-md bg-teal-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600">Add user</button>
-                </div>
-            </div>
-
+            <h2 className="text-lg font-semibold leading-6 text-zinc-700 mb-8">Requests</h2>
 
             <div className="px-4 sm:px-6 lg:px-8 border rounded-md py-4">
-
                 <div className="mt-8 flow-root">
                     <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
@@ -27,10 +18,6 @@ export default function IncomingRequests() {
                                         <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-zinc-800">Role</th>
                                         <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-zinc-800">Check In</th>
                                         <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-zinc-800">Image</th>
-                                        <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-zinc-800">Access</th>
-                                        <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                                            <span className="sr-only">Access</span>
-                                        </th>
                                     </tr>
                                 </thead>
                                 <tbody className="divide-y divide-zinc-200">
@@ -39,12 +26,14 @@ export default function IncomingRequests() {
                                         <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">Sabina Rasulova</td>
                                         <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">Front-end Developer</td>
                                         <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500"><span className="date">3/6/2024</span><span className="time">  5:19</span> <span className="PM/AM">PM</span></td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">img</td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">Registered</td>
-                                        <td className="flex whitespace-nowrap py-4 px-3 sm:pr-0 gap-2">
+                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">
+                                            <button className="text-teal-500 hover:text-red-400">View img</button>
+                                        </td>
+
+                                        {/* <td className="flex whitespace-nowrap py-4 px-3 sm:pr-0 gap-2">
                                             <button className="bg-red-300 hover:bg-red-400 text-sm text-white py-1 px-3 rounded-md">Allow</button>
                                             <button className="bg-teal-700 hover:bg-teal-600 text-sm text-white py-1 px-3 rounded-md">Deny</button>
-                                        </td>
+                                        </td> */}
                                     </tr>
 
                                     <tr>
@@ -52,11 +41,8 @@ export default function IncomingRequests() {
                                         <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">Kenneth Blanton</td>
                                         <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">Front-end Developer</td>
                                         <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500"><span className="date">3/6/2024</span><span className="time">  5:19</span> <span className="PM/AM">PM</span></td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">img</td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">Registered</td>
-                                        <td className="flex whitespace-nowrap py-4 px-3 sm:pr-0 gap-2">
-                                            <button className="bg-red-300 hover:bg-red-400 text-sm text-white py-1 px-3 rounded-md">Allow</button>
-                                            <button className="bg-teal-700 hover:bg-teal-600 text-sm text-white py-1 px-3 rounded-md">Deny</button>
+                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">
+                                            <button className="text-teal-500 hover:text-red-500">View img</button>
                                         </td>
                                     </tr>
 
@@ -65,11 +51,8 @@ export default function IncomingRequests() {
                                         <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">Grace Joh</td>
                                         <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">Software Engineer</td>
                                         <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500"><span className="date">3/6/2024</span><span className="time">  5:19</span> <span className="PM/AM">PM</span></td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">img</td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">Registered</td>
-                                        <td className="flex whitespace-nowrap py-4 px-3 sm:pr-0 gap-2">
-                                            <button className="bg-red-300 hover:bg-red-400 text-sm text-white py-1 px-3 rounded-md">Allow</button>
-                                            <button className="bg-teal-700 hover:bg-teal-600 text-sm text-white py-1 px-3 rounded-md">Deny</button>
+                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">
+                                            <button className="text-teal-500 hover:text-red-400">View img</button>
                                         </td>
                                     </tr>
 
--- wombat-admin/src/components/statistics.tsx
+++ wombat-admin/src/components/statistics.tsx
@@ -1,37 +1,43 @@
 export default function Statistics() {
     return (
-        <div className="">
-            <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-6">
-                <div className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
-                    <dt className="truncate text-sm font-medium text-zinc-500">Total Visitors</dt>
-                    <dd className="mt-1 text-3xl font-semibold tracking-tight text-red-400">128</dd>
-                </div>
+        <div>
 
-                <div className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
-                    <dt className="truncate text-sm font-medium text-zinc-500">Default Accesses</dt>
-                    <dd className="mt-1 text-3xl font-semibold tracking-tight text-red-400">110</dd>
-                </div>
+            <h2 className="text-lg font-semibold leading-6 text-zinc-700 mb-8">Statistics</h2>
 
-                <div className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
-                    <dt className="truncate text-sm font-medium text-zinc-500">Admin Accesses</dt>
-                    <dd className="mt-1 text-3xl font-semibold tracking-tight text-red-400">18</dd>
-                </div>
+            <div className="border rounded-md p-4">
+                <dl className="grid grid-cols-1 gap-5 sm:grid-cols-2 ">
+                    <div className="overflow-hidden rounded-lg bg-white shadow-md p-3">
+                        <dt className="truncate text-sm font-medium text-zinc-500">Total Visitors</dt>
+                        <dd className="mt-1 text-2xl font-semibold tracking-tight text-red-400">128</dd>
+                    </div>
 
-                <div className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
-                    <dt className="truncate text-sm font-medium text-zinc-500">Present Users</dt>
-                    <dd className="mt-1 text-3xl font-semibold tracking-tight text-red-400">58</dd>
-                </div>
-                <div className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
-                    <dt className="truncate text-sm font-medium text-zinc-500">Absent Users</dt>
-                    <dd className="mt-1 text-3xl font-semibold tracking-tight text-red-400">70</dd>
-                </div>
+                    <div className="overflow-hidden rounded-lg bg-white shadow-md  p-3">
+                        <dt className="truncate text-sm font-medium text-zinc-500">Default Accesses</dt>
+                        <dd className="mt-1 text-2xl font-semibold tracking-tight text-red-400">110</dd>
+                    </div>
 
-                <div className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
-                    <dt className="truncate text-sm font-medium text-zinc-500">Denied Accesses</dt>
-                    <dd className="mt-1 text-3xl font-semibold tracking-tight text-red-400">4</dd>
-                </div>
-            </dl>
+                    <div className="overflow-hidden rounded-lg bg-white shadow-md  p-3">
+                        <dt className="truncate text-sm font-medium text-zinc-500">Admin Accesses</dt>
+                        <dd className="mt-1 text-2xl font-semibold tracking-tight text-red-400">18</dd>
+                    </div>
+
+                    <div className="overflow-hidden rounded-lg bg-white shadow-md  p-3">
+                        <dt className="truncate text-sm font-medium text-zinc-500">Present Users</dt>
+                        <dd className="mt-1 text-2xl font-semibold tracking-tight text-red-400">58</dd>
+                    </div>
+                    <div className="overflow-hidden rounded-lg bg-white shadow-md  p-3">
+                        <dt className="truncate text-sm font-medium text-zinc-500">Absent Users</dt>
+                        <dd className="mt-1 text-2xl font-semibold tracking-tight text-red-400">70</dd>
+                    </div>
+
+                    <div className="overflow-hidden rounded-lg bg-white shadow-md  p-3">
+                        <dt className="truncate text-sm font-medium text-zinc-500">Denied Accesses</dt>
+                        <dd className="mt-1 text-2xl font-semibold tracking-tight text-red-400">4</dd>
+                    </div>
+                </dl>
+            </div>
         </div>
+
 
     )
 }
