3557bef22bf56c7f511f49d80f8991eeccc85f38
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-03-28 16:50:43 -0400

    WIP: update styling

--- wombat-admin/src/components/dashboard.tsx
+++ wombat-admin/src/components/dashboard.tsx
@@ -23,20 +23,27 @@ export default function Dashboard() {
                 </div>
             </header>
 
+            <div className=" mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-4  sm:flex-none flex justify-end">
+                <button type="button" className="block rounded-md bg-teal-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600">Add user</button>
+            </div>
 
             <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                <IncomingRequests />
+                <div className="grid grid-cols-3 gap-4">
+                    <div className="col-span-2">
+                        <IncomingRequests />
+                    </div>
+                    <Statistics />
+                </div>
 
-                <h1 className="text-lg font-semibold leading-6 text-zinc-800 mt-10 mb-4">Reports</h1>
+
+                <h2 className="text-lg font-semibold leading-6 text-zinc-800 mt-10 mb-4">Reports</h2>
 
                 <div className="sm:hidden">
                     <label htmlFor="tabs" className="sr-only">Select a tab</label>
                     {/* <!-- Use an "onChange" listener to redirect the user to the selected tab URL. --> */}
-                    <select id="tabs" name="tabs" className="block w-full rounded-md border-zinc-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-                        <option>Daily Visits</option>
+                    <select id="tabs" name="tabs" className="block w-full rounded-md border-zinc-300 py-2 pl-3 pr-10 text-base focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm">
+                        <option selected>Daily Visits</option>
                         <option>User Audit Trail</option>
-                        <option selected>Statistics</option>
-
                     </select>
                 </div>
                 <div className="hidden sm:block">
@@ -45,13 +52,10 @@ export default function Dashboard() {
                             {/* <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-zinc-500 hover:border-zinc-300 hover:text-zinc-700" --> */}
                             <a href="#" className=" border-teal-500 text-teal-600  hover:border-zinc-300 hover:text-zinc-700 whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium">Daily Visits</a>
                             <a href="#" className="border-transparent text-zinc-500 hover:border-zinc-300 hover:text-zinc-700 whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium">User Audit Trail</a>
-                            <a href="#" className="border-transparent text-zinc-500 whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium" aria-current="page">Statistics</a>
-
                         </nav>
                     </div>
                 </div>
-                {/* <DailyVisits /> */}
-                <Statistics />
+                <DailyVisits />
             </div>
 
         </div>
