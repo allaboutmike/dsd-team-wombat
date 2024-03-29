5d4805c3cd4a7052e8a598c8e65f4035e91b0e63
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-03-29 19:39:38 -0400

    add toggle modal functionality

--- wombat-admin/src/app/page.tsx
+++ wombat-admin/src/app/page.tsx
@@ -1,15 +1,26 @@
+"use client"
+
 import Image from "next/image";
+import { useState } from "react";
 import Dashboard from "@/components/dashboard";
 import Navbar from "@/components/navbar";
+import AddUser from "@/components/add_user";
 
 export default function Home() {
+
+  const [addUserModal, setAddUserModal] = useState(false);
+
+  const toggleAddUserModal = () => {
+    setAddUserModal((prev) => !prev);
+  };
+
   return (
     <main className="min-h-full">
       <Navbar />
-      <Dashboard />
+      <Dashboard toggleAddUserModal={toggleAddUserModal} />
+      {addUserModal && <AddUser toggleAddUserModal={toggleAddUserModal} />}
+
     </main>
-
-
 
   );
 }
