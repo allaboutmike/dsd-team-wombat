cd6dfa0875b39e677ec31756f627134fc1857de0
Amanda Perry <challenge641@gmail.com>
2024-03-31 14:10:42 -0500

    [SCRUM-41]updating the user page with design elements to match admin page

--- user/src/App.tsx
+++ user/src/App.tsx
@@ -1,13 +1,14 @@
 // import { useState } from "react";
 import "./App.css";
 import WebcamCapture from "./Components/Webcam";
+import Nav from "./Components/Nav";
 
 function App() {
 
   return (
     <>
-      <h1 className="text-8xl bg-clip-text text-transparent bg-gradient-to-b from-gray-300 to-gray-700">Wombat Visage</h1>
-      <h1 className="text-3xl">center yourself - look into camera - capture</h1>
+      <Nav />
+      <h2 className="text-lg font-semibold leading-6 text-zinc-700">center yourself - look into camera - capture</h2>
       <WebcamCapture />
 
     </>
--- user/src/Components/Webcam.tsx
+++ user/src/Components/Webcam.tsx
@@ -39,7 +39,7 @@ const WebcamCapture = () => {
   return (
     <>
       <form>
-        <div className="flex">
+        <div className="flex mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
           <div>
             <Webcam
               audio={false}
@@ -48,29 +48,35 @@ const WebcamCapture = () => {
             />
           </div>
 
-          <div>{imgSrc && <img src={imgSrc} />}</div>
+          <div>
+            {imgSrc && <img src={imgSrc} />}
+          </div>
         </div>
 
-        <button onClick={capture} type="button">
-          Capture photo
-        </button>
+        <div className=" mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-4  sm:flex-none flex justify-center">
+          <button onClick={capture} type="button" className="block rounded-md bg-teal-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600">
+            Capture photo
+          </button>
+        
 
-        {taken ? (
-          <>
-            <input
-              type="text"
-              value={badgeId}
-              onChange={(e) => {
-                setBadgeId(e.target.value);
-              }}
-            />
-            <button type="submit" onClick={handleSubmit}>
-              Submit
-            </button>
-
+          {taken ? (
+            <>
+              <input
+                type="text"
+                value={badgeId}
+                onChange={(e) => {
+                  setBadgeId(e.target.value);
+                }}
+              />
+              
+                <button type="submit" className="block rounded-md bg-teal-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600" onClick={handleSubmit}>
+                  Submit
+                </button>            
+            
             <p>{error}</p>
           </>
         ) : null}
+        </div>
       </form>
     </>
   );
