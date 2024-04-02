7d86ab66bb000182ef2e01f1823460ac62836a80
Joshua Ow <joshuaow@gmail.com>
2024-04-02 11:56:49 -0500

    the access method will check the user id and find a related image, then send the FR the client image and db image to verify, also added correct headers to api request on front end (SCRUM-63)

--- admin_server/src/main/java/com/backend/admin_server/config/SecurityConfig.java
+++ admin_server/src/main/java/com/backend/admin_server/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -25,6 +26,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors(cors -> cors.configurationSource(corsConfig()))
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/access_request", "/**").permitAll()
