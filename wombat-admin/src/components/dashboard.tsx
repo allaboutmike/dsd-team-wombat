cde8b4bb99f736106a1cbd15c2ccb7db8271e42f
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-04-02 17:04:34 -0400

    Add functionality to toggle between nav tabs

--- wombat-admin/src/components/dashboard.tsx
+++ wombat-admin/src/components/dashboard.tsx
@@ -1,3 +1,4 @@
+import AuditTrail from "./audit_trail";
 import DailyVisits from "./daily_visits";
 import IncomingRequests from "./incoming_requests";
 import Statistics from "./statistics";
@@ -6,10 +7,12 @@ import Statistics from "./statistics";
 type DashboardProps = {
     toggleAddUserModal: () => void;
     toggleViewImageModal: () => void;
+    activeTab: string;
+    handleTabClick: (tabName: string) => void;
 };
 
-export default function Dashboard({ toggleAddUserModal, toggleViewImageModal }: DashboardProps) {
-
+export default function Dashboard({ toggleAddUserModal, toggleViewImageModal, activeTab, handleTabClick }: DashboardProps) {
+    // console.log('Active Tab:', activeTab);
     return (
         <div>
             <header className="bg-white shadow-sm">
@@ -36,20 +39,21 @@ export default function Dashboard({ toggleAddUserModal, toggleViewImageModal }: 
                 <div className="sm:hidden">
                     <label htmlFor="tabs" className="sr-only">Select a tab</label>
                     {/* <!-- Use an "onChange" listener to redirect the user to the selected tab URL. --> */}
-                    <select id="tabs" name="tabs" className="block w-full rounded-md border-zinc-300 py-2 pl-3 pr-10 text-base focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm">
-                        <option selected>Daily Visits</option>
-                        <option>User Audit Trail</option>
+                    <select id="tabs" name="tabs" className="block w-full rounded-md border-zinc-300 py-2 pl-3 pr-10 text-base focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm" value={activeTab} onChange={(e) => handleTabClick(e.target.value)}>
+                        <option selected value="Daily Visits">Daily Visits</option>
+                        <option value="User Audit Trail">User Audit Trail</option>
                     </select>
                 </div>
                 <div className="hidden sm:block">
                     <div className="">
                         <nav className="-mb-px flex space-x-8" aria-label="Tabs">
-                            <a href="#" className=" border-teal-500 text-teal-600  hover:border-zinc-300 hover:text-zinc-700 whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium">Daily Visits</a>
-                            <a href="#" className="border-transparent text-zinc-500 hover:border-zinc-300 hover:text-zinc-700 whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium">User Audit Trail</a>
+                            <a href="#" className={`whitespace-nowrap py-4 px-1 text-sm font-medium ${activeTab === 'Daily Visits' ? 'border-b-2 border-teal-500 text-teal-600' : 'border-transparent text-zinc-500'}`} onClick={() => handleTabClick('Daily Visits')}>Daily Visits</a>
+                            <a href="#" className={` whitespace-nowrap py-4 px-1 text-sm font-medium ${activeTab === 'User Audit Trail' ? 'border-b-2 border-teal-500 text-teal-600' : 'border-transparent text-zinc-500'}`} onClick={() => handleTabClick('User Audit Trail')}>User Audit Trail</a>
                         </nav>
                     </div>
                 </div>
-                <DailyVisits />
+                {activeTab === 'Daily Visits' && <DailyVisits />}
+                {activeTab === 'User Audit Trail' && <AuditTrail />}
             </div>
 
         </div>
