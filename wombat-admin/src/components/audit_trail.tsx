a2324761e0c9a7cb496e9436b41005345dd8f5e4
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-04-02 17:16:09 -0400

    add more details to audit trail

--- wombat-admin/src/components/audit_trail.tsx
+++ wombat-admin/src/components/audit_trail.tsx
@@ -1,7 +1,7 @@
 export default function AuditTrail() {
     return (
         <div>
-            <div className="px-2 border rounded-md ">
+            <div className="px-2 border rounded-md overflow-scroll ">
                 <div className="sm:flex sm:items-center">
                 </div>
                 <div className="mt-8 flow-root">
@@ -15,12 +15,12 @@ export default function AuditTrail() {
                                         <th scope="col" className="px-4 py-3.5 text-left text-sm font-semibold text-zinc-800">Name</th>
                                         <th scope="col" className="px-4 py-3.5 text-left text-sm font-semibold text-zinc-800">Role</th>
                                         <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-zinc-800">Check Ins</th>
+                                        <th scope="col" className="px-4 py-3.5 text-left text-sm font-semibold text-zinc-800">Access Given By</th><th scope="col" className="px-4 py-3.5 text-left text-sm font-semibold text-zinc-800">Access Type</th>
                                         <th scope="col" className="px-4 py-3.5 text-left text-sm font-semibold text-zinc-800">Image</th>
                                         <th scope="col" className="px-4 py-3.5 text-left text-sm font-semibold text-zinc-800">Status</th>
                                         <th scope="col" className="px-4 py-3.5 text-left text-sm font-semibold text-zinc-800">Check Out</th>
                                         <th scope="col" className="px-4 py-3.5 text-left text-sm font-semibold text-zinc-800">Registered At Date&Time</th>
-                                        <th scope="col" className="px-4 py-3.5 text-left text-sm font-semibold text-zinc-800">Registered by Admin</th>
-                                        <th scope="col" className="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-zinc-800 sm:pr-0">Access Type</th>
+                                        <th scope="col" className="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-zinc-800 sm:pr-0">Registered by Admin</th>
                                     </tr>
                                 </thead>
                                 <tbody className="divide-y divide-zinc-200 bg-white">
@@ -34,12 +34,13 @@ export default function AuditTrail() {
                                         <td className="whitespace-nowrap p-4 text-sm text-zinc-500">Aliya Rodriguez</td>
                                         <td className="whitespace-nowrap p-4 text-sm text-zinc-500">Front-end Developer</td>
                                         <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500"><span className="date">3/6/2024</span><span className="time">  5:19</span> <span className="PM/AM">PM</span></td>
+                                        <td className="whitespace-nowrap p-4 text-sm text-zinc-500">Noel</td>
+                                        <td className="whitespace-nowrap p-4 text-sm text-teal-500">Default</td>
                                         <td className="whitespace-nowrap p-4 text-sm text-zinc-500">img</td>
                                         <td className="whitespace-nowrap p-4 text-sm text-teal-500">Present</td>
                                         <td className="whitespace-nowrap p-4 text-sm text-zinc-500"><span className="date">3/6/2024</span><span className="time">  6:30</span> <span className="PM/AM">PM</span></td>
                                         <td className="whitespace-nowrap p-4 text-sm text-zinc-500"><span className="date">2/8/2024</span><span className="time">  12:30</span> <span className="PM/AM">PM</span></td>
-                                        <td className="whitespace-nowrap p-4 text-sm text-zinc-500">Mike</td>
-                                        <td className="whitespace-nowrap py-4 pl-4 pr-4 text-sm text-teal-500 sm:pr-0">Default</td>
+                                        <td className="whitespace-nowrappy-4 pl-4 pr-4 text-sm text-zinc-500 sm:pr-0">Mike</td>
                                     </tr>
 
                                     <tr className="divide-x divide-zinc-200">
@@ -52,12 +53,13 @@ export default function AuditTrail() {
                                         <td className="whitespace-nowrap p-4 text-sm text-zinc-500">Aliya Rodriguez</td>
                                         <td className="whitespace-nowrap p-4 text-sm text-zinc-500">Front-end Developer</td>
                                         <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500"><span className="date">3/5/2024</span><span className="time">  9:02</span> <span className="PM/AM">AM</span></td>
+                                        <td className="whitespace-nowrap p-4 text-sm text-zinc-500">Noel</td>
+                                        <td className="whitespace-nowrap p-4 text-sm text-teal-500">Default</td>
                                         <td className="whitespace-nowrap p-4 text-sm text-zinc-500">img</td>
                                         <td className="whitespace-nowrap p-4 text-sm text-red-400">Absent</td>
                                         <td className="whitespace-nowrap p-4 text-sm text-zinc-500"><span className="date">3/5/2024</span><span className="time">  6:30</span> <span className="PM/AM">PM</span></td>
                                         <td className="whitespace-nowrap p-4 text-sm text-zinc-500"><span className="date">2/8/2024</span><span className="time">  12:30</span> <span className="PM/AM">PM</span></td>
-                                        <td className="whitespace-nowrap p-4 text-sm text-zinc-500">Mike</td>
-                                        <td className="whitespace-nowrap py-4 pl-4 pr-4 text-sm text-teal-500 sm:pr-0">Default</td>
+                                        <td className="whitespace-nowrappy-4 pl-4 pr-4 text-sm text-zinc-500 sm:pr-0">Mike</td>
                                     </tr>
 
                                     <tr className="divide-x divide-zinc-200">
@@ -70,12 +72,13 @@ export default function AuditTrail() {
                                         <td className="whitespace-nowrap p-4 text-sm text-zinc-500">Kenneth Blanton</td>
                                         <td className="whitespace-nowrap p-4 text-sm text-zinc-500">Full Stack Developer</td>
                                         <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500"><span className="date">3/6/2024</span><span className="time">  5:19</span> <span className="PM/AM">PM</span></td>
+                                        <td className="whitespace-nowrap p-4 text-sm text-zinc-500">Noel</td>
+                                        <td className="whitespace-nowrap p-4 text-sm text-red-400">Manual</td>
                                         <td className="whitespace-nowrap p-4 text-sm text-zinc-500">img</td>
                                         <td className="whitespace-nowrap p-4 text-sm text-red-500">Absent</td>
                                         <td className="whitespace-nowrap p-4 text-sm text-zinc-500"><span className="date">3/6/2024</span><span className="time">  6:30</span> <span className="PM/AM">PM</span></td>
                                         <td className="whitespace-nowrap p-4 text-sm text-zinc-500"><span className="date">1/15/2024</span><span className="time">  9:20</span> <span className="PM/AM">AM</span></td>
-                                        <td className="whitespace-nowrap p-4 text-sm text-zinc-500">Mike</td>
-                                        <td className="whitespace-nowrap py-4 pl-4 pr-4 text-sm text-red-400 sm:pr-0">Manual</td>
+                                        <td className="whitespace-nowrappy-4 pl-4 pr-4 text-sm text-zinc-500 sm:pr-0">Mike</td>
                                     </tr>
 
                                     {/* <!-- More people... --> */}
