216838979c9688863b61ca2ced1be8e40f7d9d07
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-04-05 12:00:11 -0400

    add models

--- wombat-admin/src/app/models/models.tsx
+++ wombat-admin/src/app/models/models.tsx
@@ -2,16 +2,34 @@ type User = {
     id: number,
     firstName: string,
     lastName: string,
-    image: string,
+    username: string,
+    image: {
+        img1: string,
+        img2: string,
+        img3: string,
+        img4: string,
+        img5: string
+    },
     role: string,
     department: string,
-    registeredByAdmin: string,
+    registeredByAdmin: string, //admin username
     registerDay: string,
     registerTime: string,
+
+}
+
+type UserVisit = {
+    id: number,
     checkInDay: string,
     checkInHour: string,
     checkOutDay: string,
     access: null | boolean,
     accessType: string, // default or manual
     status: boolean // present or absent
+    accessGivenBy: string // admin username
+}
+
+type Admin = {
+    id: number,
+    username: string
 }
