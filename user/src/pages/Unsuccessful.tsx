e5c880370563dd7052b42590edaddda4cfce69a3
Amanda Perry <challenge641@gmail.com>
2024-04-07 10:28:07 -0500

    added spinner to admin request override modal

--- user/src/pages/Unsuccessful.tsx
+++ user/src/pages/Unsuccessful.tsx
@@ -2,16 +2,18 @@ import VerifyModal from "../Components/Modal";
 import { openModal } from "../Components/Modal";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import LoadingSpinner from "../Components/LoadingSpinner";
 
 const Unsuccessful = () => {
   const [badgeId, setBadgeId] = useState("");
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const navigate = useNavigate();
 
   async function handleSubmit(e: any) {
     e.preventDefault();
-
+    setIsLoading(true)
     const headers = {
       "Content-Type": "application/json",
       Accept: "application/json",
@@ -28,7 +30,8 @@ const Unsuccessful = () => {
     });
 
     const data = await res.json();
-
+    setIsLoading(false)
+    
     if (data.error) {
       return setError(data.error);
     }
@@ -40,44 +43,50 @@ const Unsuccessful = () => {
     }
   }
   return (
-    <>
-      <div>
-        <button
-          type="button"
-          onClick={() => openModal("verify_modal")}
-          className="rounded-md bg-teal-600 px-3 py-2 my-4 text-center text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
-        >
-          Request Admin Verification
-        </button>
-      </div>
-
-      <VerifyModal id="verify_modal">
-        <h1 className="text-2xl font-semibold mb-7 text-white">
-          Enter your information
-        </h1>
-        <form className="w-[450px]">
-          <div className="mb-5">
-            <input
-              className="w-full py-2 px-4 rounded bg-[#4b5563] text-white mb-5 focus:outline-none"
-              type="text"
-              placeholder="Enter BadgeID"
-              value={badgeId}
-              onChange={(e) => {
-                setBadgeId(e.target.value);
-              }}
-            />
-          </div>
-
+    <> { isLoading ? (
+      <LoadingSpinner />
+    ) : ( 
+      <>
+        <div>
           <button
-            type="submit"
-            onClick={handleSubmit}
-            className="w-full block rounded-md bg-teal-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
+            type="button"
+            onClick={() => openModal("verify_modal")}
+            className="rounded-md bg-teal-600 px-3 py-2 my-4 text-center text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
           >
-            Request Admin Override
+            Request Admin Verification
           </button>
-        </form>
-      </VerifyModal>
+        </div>
 
+        <VerifyModal id="verify_modal">
+          <h1 className="text-2xl font-semibold mb-7 text-white">
+            Enter your information
+          </h1>
+          <form className="w-[450px]">
+            <div className="mb-5">
+              <input
+                className="w-full py-2 px-4 rounded bg-[#4b5563] text-white mb-5 focus:outline-none"
+                type="text"
+                placeholder="Enter BadgeID"
+                value={badgeId}
+                onChange={(e) => {
+                  setBadgeId(e.target.value);
+                }}
+              />
+            </div>
+
+            <button
+              type="submit"
+              onClick={handleSubmit}
+              className="w-full block rounded-md bg-teal-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
+            >
+              Request Admin Override
+            </button>
+          </form>
+        </VerifyModal>
+
+      </>
+    )}
+      
       <p>{error}</p>
     </>
   );
