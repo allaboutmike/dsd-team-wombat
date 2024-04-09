f81c66258a65319455413033d7e2ea0df80f8bad
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2024-04-09 13:41:06 -0400

    SCRUM-83 [Chore] Added Capture to route

--- user/src/App.tsx
+++ user/src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import WebcamCapture from "./Components/Webcam";
 import Home from "./pages/Home";
 import Nav from "./Components/Nav";
 import { Route, Routes } from "react-router-dom";
 import Successful from "./pages/Successful";
 import Unsuccessful from "./pages/Unsuccessful";
+import Capture from "./pages/Capture";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Capture" element={<WebcamCapture />} />
+        <Route path="/Capture" element={<Capture />} />
         <Route path="/Successful" element={<Successful />} />
         <Route path="/Unsuccessful" element={<Unsuccessful />} />
         <Route path="/*" element={<Home />} />
@@ -22,5 +22,3 @@ function App() {
 }
 
 export default App;
-
-
