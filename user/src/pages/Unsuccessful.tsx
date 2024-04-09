8ac0233099be8312b79cacc216f45b65f2d0e1e8
Amanda Perry <challenge641@gmail.com>
2024-04-09 12:24:57 -0500

    swapped out modal for plain form on unsuccessful paige

--- user/src/pages/Unsuccessful.tsx
+++ user/src/pages/Unsuccessful.tsx
@@ -1,5 +1,3 @@
-import VerifyModal from "../Components/Modal";
-import { openModal } from "../Components/Modal";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import LoadingSpinner from "../Components/LoadingSpinner";
@@ -50,17 +48,7 @@ const Unsuccessful = () => {
         <LoadingSpinner />
       ) : (
         <>
-          <div>
-            <button
-              type="button"
-              onClick={() => openModal("verify_modal")}
-              className="rounded-md bg-teal-600 px-3 py-2 my-4 text-center text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
-            >
-              Request Admin Verification
-            </button>
-          </div>
 
-          <VerifyModal id="verify_modal">
             <h1 className="text-2xl font-semibold mb-7 text-white">
               Enter your information
             </h1>
@@ -85,7 +73,7 @@ const Unsuccessful = () => {
                 Request Admin Override
               </button>
             </form>
-          </VerifyModal>
+   
         </>
       )}
       <p>{error}</p>
