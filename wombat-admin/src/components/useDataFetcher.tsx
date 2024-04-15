3530509bbf6a63062a0c5b27444600b5e4018988
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-04-15 19:25:36 -0400

    modify the custom hook to accept filtering fn

--- wombat-admin/src/components/useDataFetcher.tsx
+++ wombat-admin/src/components/useDataFetcher.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const useDataFetcher = (url: string) => {
+const useDataFetcher = (url: string, filterFn?: any) => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
@@ -11,13 +11,19 @@ const useDataFetcher = (url: string) => {
                     throw new Error('Failed to fetch data');
                 }
                 const responseData = await response.json();
-                setData(responseData);
+                if (filterFn) {
+                    setData(filterFn(responseData))
+                }
+                else {
+                    setData(responseData)
+                }
+
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         };
         fetchData();
-    }, [url]);
+    }, [url, filterFn]);
 
     return data;
 };
