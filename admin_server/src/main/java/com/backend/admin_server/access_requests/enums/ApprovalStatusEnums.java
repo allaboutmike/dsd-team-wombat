017e4970d5b32a491af04f928f100cf975524767
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2024-04-15 22:49:18 -0400

    started on clean up

--- admin_server/src/main/java/com/backend/admin_server/access_requests/enums/ApprovalStatusEnums.java
+++ admin_server/src/main/java/com/backend/admin_server/access_requests/enums/ApprovalStatusEnums.java
@@ -1,7 +1,12 @@
 package com.backend.admin_server.access_requests.enums;
 
 public enum ApprovalStatusEnums {
+<<<<<<< Updated upstream
     APPROVED,
     DENIED,
 
 }
+=======
+
+}
+>>>>>>> Stashed changes
