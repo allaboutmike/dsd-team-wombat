a579d70ee2b09bae23970d10815b6a6adb029eb9
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2024-04-16 00:05:26 -0400

    fixed enum

--- admin_server/src/main/java/com/backend/admin_server/access_requests/enums/ApprovalStatusEnums.java
+++ admin_server/src/main/java/com/backend/admin_server/access_requests/enums/ApprovalStatusEnums.java
@@ -1,12 +1,7 @@
 package com.backend.admin_server.access_requests.enums;
 
 public enum ApprovalStatusEnums {
-<<<<<<< Updated upstream
     APPROVED,
     DENIED,
 
 }
-=======
-
-}
->>>>>>> Stashed changes
