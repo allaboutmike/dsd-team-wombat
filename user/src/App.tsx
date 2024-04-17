5ae380467777b916a084689394fcd1032de5c70d
Amanda Perry <challenge641@gmail.com>
2024-04-17 18:38:20 -0500

    added taggle for light and dark mode with basic design changes, updated logo and title

--- user/src/App.tsx
+++ user/src/App.tsx
@@ -1,19 +1,43 @@
 import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Unsuccessful from "./pages/Unsuccessful";
 import Successful from "./pages/Successful";
 import Capture from "./pages/Capture";
 import Nav from "./Components/Nav";
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  useEffect(() => {
+    let savedMode = localStorage.getItem("displayMode");
+    if (!savedMode) {
+      savedMode = "light"; //can change this to automatically set to browser's default mode
+      setDarkMode(false);
+      localStorage.setItem("displayMode", savedMode);
+    }
+    setDarkMode(savedMode === "dark" ? true : false);
+  }, []);
+
+  const toggleDisplayMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Capture />} />
-        <Route path="/Successful" element={<Successful />} />
-        <Route path="/Unsuccessful" element={<Unsuccessful />} />
-        <Route path="/*" element={<Successful />} />
-      </Routes>
-    </>
+    <div className={`${darkMode ? "dark" : ""}`}>
+      <div className="dark:bg-[#18214A] h-screen">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Capture />} />
+          <Route path="/Successful" element={<Successful />} />
+          <Route path="/Unsuccessful" element={<Unsuccessful />} />
+          <Route path="/*" element={<Successful />} />
+        </Routes>
+        <button
+          onClick={toggleDisplayMode}
+          className="transition duration-400 dark:bg-gray-100 dark:text-gray-700 bg-gray-700 text-gray-100 border font-bold p-3 rounded-lg"
+        >
+          mode
+        </button>
+      </div>
+    </div>
   );
 }
--- user/src/Components/Nav.tsx
+++ user/src/Components/Nav.tsx
@@ -1,10 +1,24 @@
-import logo_light from "../assets/images/WOMBAT_L.png";
+import logo_light from "../assets/images/WOMLIGHT_LOGO.png";
+import logo_light_name from "../assets/images/WOMLIGHT_NAME.png";
+import logo_dark from "../assets/images/WOMBLU_LOGO.png";
+import logo_dark_name from "../assets/images/WOMBLU_NAME.png";
 
 export default function Nav() {
   return (
     <>
-      <header className="bg-white shadow-sm">
-        <img src={logo_light} height={150} width={150} className="ml-6" />
+      <header className="transition duration-400 dark:bg-[#18214A] dark:text-gray-100 dark:shadow-blue-950 dark:shadow-lg bg-white shadow-lg flex">
+        <img
+          src={logo_dark}
+          height={150}
+          width={150}
+          className="ml-1/10 rounded-[60px]"
+        />
+        <img
+          src={logo_dark_name}
+          height={150}
+          width={350}
+          className="mx-auto"
+        />
       </header>
     </>
   );
