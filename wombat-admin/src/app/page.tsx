47415efd5bc8da9ffe21e667ee7081a4c1a2fce0
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-04-18 00:05:51 -0400

    change the logic of fetching data

--- wombat-admin/src/app/page.tsx
+++ wombat-admin/src/app/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import Dashboard from "@/components/dashboard";
 import AddUser from "@/components/add_user";
 import ViewImageForAccess from "@/components/view_img_for_access";
-import useDataFetcher from "@/components/useDataFetcher";
-import { IncomingRequest } from "@/app/models/models";
+import useDataFetcher from "@/app/util/useDataFetcher";
+import { IncomingRequest, User } from "@/app/models/models";
+
+// TODO: move to env
+const URL = 'http://localhost:4040'
+
+
 
 export default function Home() {
 
@@ -17,8 +22,25 @@ export default function Home() {
   const [activeTab, setActiveTab] = useState<string>('Daily Visits');
   const [currentPage, setCurrentPage] = useState(1);
 
+  const manualOverridenRequests = incomingDeniedRequests.filter(r => r.state === "MANUAL_OVERRIDE_REQUESTED");
 
+  useEffect(() => {
 
+    async function fetchData() {
+      const res = await fetch(`${URL}/access_request`);
+      if (!res.ok) {
+        console.error(res);
+        return;
+      }
+
+      const reqs: IncomingRequest[] = await res.json();
+      setIncomingDeniedRequests(reqs);
+    }
+
+    fetchData();
+
+    return () => setIncomingDeniedRequests([]);
+  }, []);
 
   // Button click to show the "Add User Modal"
   const toggleAddUserModal = () => {
@@ -42,18 +64,15 @@ export default function Home() {
     setActiveTab(tabName);
   };
 
-  // Custom hook for fetching requests data
-  const filterDeniedRequests = (requests: IncomingRequest[]) => requests.filter(request => request.state === "MANUAL_OVERRIDE_REQUESTED")
-  const deniedRequests: IncomingRequest[] | null = useDataFetcher('http://localhost:4040/access_request', filterDeniedRequests);
 
   // Custom hook for fetching users data
-  const users = useDataFetcher('http://localhost:4040/users');
+  //const users = useDataFetcher('http://localhost:4040/users');
+  const users: IncomingRequest[] = [];
 
   // This function displays the view image modal for each request/visit
   const showViewImageModal = (requestId: string): void => {
-    for (let request of deniedRequests!) {
-      if (request.requestId = requestId) {
-        console.log(request.requestId, request.approvalStatus, request.state)
+    for (let request of manualOverridenRequests!) {
+      if (request.requestId === requestId) {
         setSelectedRequest(request)
         break;
       }
@@ -63,38 +82,15 @@ export default function Home() {
   const closeViewImageModal = (): void => setSelectedRequest(null)
 
 
-  const updateRequest = async (request: IncomingRequest): Promise<void> => {
-    try {
-      await fetch(`http://localhost:4040/access_request/${request.requestId}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(request),
-      });
-
-      if (selectedRequest) {
-        setSelectedRequest(request);
-      }
-    } catch (error) {
-      console.error('Error updating request:', error);
-      // Handle the error, e.g., display an error message or retry updating
-    }
-  };
-
-  const approveRequest = async (requestId: string): Promise<void> => {
-    const request = deniedRequests.find((req) => req.requestId === requestId);
+  const approveRequest = async (requestId: string) => {
+    const request = manualOverridenRequests.find((req) => req.requestId === requestId);
 
     if (!request) {
       console.error('Request not found');
       return;
     }
-
-    // Update the request state
     request.state = "MANUAL_OVERRIDE_ACTIONED";
-
     try {
-      // Create a PUT request to update the request in the database
       const response = await fetch(`http://localhost:4040/access_request/${requestId}`, {
         method: 'PUT',
         headers: {
@@ -108,13 +104,12 @@ export default function Home() {
       }
 
       console.log('Request state updated successfully');
-      // Optionally, update the local state or perform other actions if needed
+
     } catch (error) {
       console.error('Error updating request:', error);
-      // Handle the error, e.g., display an error message or retry updating
+
     }
   };
-
 
 
 
@@ -122,17 +117,17 @@ export default function Home() {
   const itemsPerPage = 3;
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentRequests = deniedRequests.slice(indexOfFirstItem, indexOfLastItem);
-  const totalPages = Math.ceil(deniedRequests.length / itemsPerPage);
+  const currentRequests = manualOverridenRequests.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(manualOverridenRequests.length / itemsPerPage);
   const nextPage = () => setCurrentPage(currentPage + 1);
   const prevPage = () => setCurrentPage(currentPage - 1);
 
   return (
     <main className="min-h-full">
-      <Dashboard activeTab={activeTab} requests={deniedRequests} users={users} currentPage={currentPage}
+      <Dashboard activeTab={activeTab} requests={manualOverridenRequests} users={users} currentPage={currentPage}
         nextPage={nextPage}
         prevPage={prevPage}
-        currentRequests={currentRequests}
+        currentRequests={manualOverridenRequests}
         totalPages={totalPages}
         handleTabClick={handleTabClick}
         toggleAddUserModal={toggleAddUserModal}
@@ -140,7 +135,7 @@ export default function Home() {
         selectedRequest={selectedRequest} />
 
       {addUserModal && <AddUser handleFileChange={handleFileChange} imageSrc={imageSrc} toggleAddUserModal={toggleAddUserModal} />}
-      {selectedRequest != null && <ViewImageForAccess onCloseViewImageModal={closeViewImageModal} onApproveRequest={approveRequest} selectedRequest={selectedRequest} />}
+      {selectedRequest && <ViewImageForAccess onCloseViewImageModal={closeViewImageModal} onApproveRequest={approveRequest} selectedRequest={selectedRequest} user={users} />}
     </main>
 
   );
